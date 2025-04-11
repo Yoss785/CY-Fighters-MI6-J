d43b772e6ffcf7fbff638031ed6b61421d99980a
Bibi110 <hugo.treny@etu.cyu.fr>
2025-04-11 11:14:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
+// Main
+int main() {
+    Personnage persos[3] = {
+        {"LUFFY", "o", 1, 10, 10, 5},
+        {"ZORO",  "z", 2, 14, 7, 38},
+        {"SANJI", "s", 3, 4, 12, 68}
+    };
 
+    afficher_plateau(persos, 3);
+    return 0;
+}
