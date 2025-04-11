bf77648eef3076a77f96b44f33cf718e0d5b8ba6
Bibi110 <hugo.treny@etu.cyu.fr>
2025-04-11 11:18:59 +0200

    Update code.h

--- code.h
+++ code.h
@@ -1,1 +1,13 @@
-
+// Structure pour stocker un personnage
+typedef struct {
+    char *nom;
+    char *effet;
+    int cible;
+    int PV;
+    int PVmax;
+    int ATQ;
+    int DEF;
+    int AGL;
+    int VIT;
+    int position;  // position horizontale du perso
+} Personnage;
