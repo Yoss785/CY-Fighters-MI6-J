e9f7e90ed134b5b2b20f9f4c085e72ff8e5d9867
Bibi110 <hugo.treny@etu.cyu.fr>
2025-04-11 11:10:36 +0200

    Update affichage.c

--- affichage.c
+++ affichage.c
@@ -11,8 +11,12 @@ typedef struct {
     char *nom;
     char *effet;
     int cible;
-    int vie;
-    int tour;
+    int PV;
+    int PVmax;
+    int ATQ;
+    int DEF;
+    int AGL;
+    int VIT;
     int position;  // position horizontale du perso
 } Personnage;
 
