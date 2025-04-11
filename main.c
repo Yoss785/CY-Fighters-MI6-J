1ac629fdb0c6e2a2957ed60ebd84ed00f771280f
Bibi110 <hugo.treny@etu.cyu.fr>
2025-04-11 09:43:33 +0000

    hvbfhvhrfbergbr

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 // Main
+#include "code.h"
 int main() {
     Personnage persos[3] = {
         {"LUFFY", "o", 1, 10, 10, 5},
         {"ZORO",  "z", 2, 14, 7, 38},
         {"SANJI", "s", 3, 4, 12, 68}
     };
-
+    printf("hello");
     afficher_plateau(persos, 3);
     return 0;
 }
