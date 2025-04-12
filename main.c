85d0f5aaf05a9646c7fe728b3b10c2ed18621fdd
Grandjean Yanis <yanisgrandjean2005@gmail.com>
2025-04-12 19:44:27 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include "code.h"
 int main() {
     Personnage persos[3] = {
-        {"LUFFY", "o", 1, 10, 10, 5},
-        {"ZORO",  "z", 2, 14, 7, 38},
-        {"SANJI", "s", 3, 4, 12, 68}
+        {"LUFFY", "l", 1, 10, 14, 10, 10, 10, 10, 5},
+        {"ZORO",  "z", 2, 14, 14, 10, 10, 10 7, 38},
+        {"SANJI", "s", 3, 4, 14, 10, 10, 10, 12, 68}
     };
     printf("hello");
     afficher_plateau(persos, 3);
