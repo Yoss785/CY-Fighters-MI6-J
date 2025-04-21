5612ba9d3aa5a71cf8ec135fa2c028564f8753c7
nyxro09 <169052725+nyxro09@users.noreply.github.com>
2025-04-21 23:28:46 +0200

    structure.h

---
+++ structure.h
@@ -0,0 +1,34 @@
+#define MAX_NOM 50
+#define MAX_DESC 200
+#define MAX_TECH 2
+
+
+// Technique spéciale d’un combattant
+typedef struct {
+    char nom[MAX_NOM];
+    int degat;                         // valeur de l’effet
+    char propriete_affectee[MAX_NOM];  // ex: "attaque", "pv", "vitesse"
+    char operation[10];                // "+", "-", "*", etc.
+    char description[MAX_DESC];
+    int tours_rechargement;           // cooldown
+    int cooldown_actuel;              // pour savoir quand elle sera dispo
+} TechniqueSpeciale;
+
+// Combattant (personnage)
+typedef struct {
+    char nom[MAX_NOM];
+
+    int pv;             // points de vie actuel
+    int pv_max;         // points de vie maximum
+    int attaque;
+    int defense;
+    int agilite;
+    int vitesse;
+
+    TechniqueSpeciale techniques[MAX_TECHNIQUES];  // techniques spéciales
+    int nb_techniques;
+
+
+    int prochain_tour; // utilisé pour déterminer quand il joue
+    int est_KO;        // 0 = actif, 1 = KO
+} Combattant;
