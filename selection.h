881c83ab089599e322c24a8b9a4a9113aae6fae5
Grandjean Yanis <yanisgrandjean2005@gmail.com>
2025-04-28 14:41:30 +0200

    Create selection.h

---
+++ selection.h
@@ -0,0 +1,13 @@
+#ifndef SELECTION_H
+#define SELECTION_H
+
+#include <stdio.h>
+#include "code.h" 
+
+void afficher_stats(Combattant perso);
+void afficher_equipe(Combattant* equipe);
+void apercu(FILE* fichier, int choix);
+void construction_perso(Combattant* perso, FILE* fichier);
+void selection(Combattant* equipe1, Combattant* equipe2, FILE* fichier);
+
+#endif
