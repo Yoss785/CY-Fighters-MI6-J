8bb09755babf870cfe123c4e6a2d2ab3f77ce1c1
Grandjean Yanis <yanisgrandjean2005@gmail.com>
2025-04-28 14:40:06 +0200

    Create techniques.h

---
+++ techniques.h
@@ -0,0 +1,9 @@
+// techniques.h
+#ifndef TECHNIQUES_H
+#define TECHNIQUES_H
+
+#include "code.h"
+
+void appliquer_technique(Combattant* utilisateur, Combattant* cible, TechniqueSpeciale* technique);
+
+#endif
