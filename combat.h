f3032d842cf48e9ee69ac905777d0a2978f1bc60
Grandjean Yanis <yanisgrandjean2005@gmail.com>
2025-05-02 09:44:41 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -1,3 +1,11 @@
+#ifndef COMBAT_H
+#define COMBAT_H
+
+#include "code.h"
+
 void phase(Combattant* equipe1,Combattant* equipe2);
 void tour(Combattant* perso,Combattant* equipe);
 void attaque(Combattant* perso,Combattant* equipe);
+void appliquer_technique(Combattant* utilisateur, Combattant* cible, TechniqueSpeciale* technique);
+
+#endif
