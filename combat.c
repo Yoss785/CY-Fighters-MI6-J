5479a151908b281bc50f01a994d39501ee13e254
Grandjean Yanis <yanisgrandjean2005@gmail.com>
2025-05-02 09:33:25 +0200

    Update combat.c

--- combat.c
+++ combat.c
@@ -55,3 +55,26 @@ void attaque(Combattant* perso,Combattant* equipe){
         (equipe+choix-1)->est_KO=0;
     }
 }
+
+void appliquer_technique(Combattant* utilisateur, Combattant* cible, TechniqueSpeciale* technique) {
+    if (technique->cooldown_actuel > 0) {
+        printf("La technique %s est en rechargement (%d tours restants).\n",
+               technique->nom, technique->cooldown_actuel);
+        return;
+    }
+
+    printf("%s utilise %s sur %s !\n", utilisateur->nom, technique->nom, cible->nom);
+    printf("Effet : %s\n", technique->description);
+
+    if (strcmp(technique->propriete_affectee, "pv") == 0) {
+        if (strcmp(technique->operation, "-") == 0) {
+            cible->pv -= technique->degat;
+            if (cible->pv < 0) cible->pv = 0;
+        } else if (strcmp(technique->operation, "+") == 0) {
+            cible->pv += technique->degat;
+            if (cible->pv > cible->pv_max) cible->pv = cible->pv_max;
+        }
+    }
+
+    technique->cooldown_actuel = technique->tours_rechargement;
+}
