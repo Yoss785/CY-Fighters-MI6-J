c9d4a5c27411f46eafb08caeed427d9218c03833
Bibi110 <hugo.treny@etu.cyu.fr>
2025-05-06 10:07:29 +0200

    Add files via upload

---
+++ check.c
@@ -0,0 +1,15 @@
+#include "code.h"
+int equipe_vivante(Combattant* equipe){
+    int morts=0;
+    for(int i=0;i<TAILLE_EQUIPE;i++){
+        if((equipe+i)->est_KO==1){
+            morts+=1;
+        }
+    }
+    if(morts==TAILLE_EQUIPE){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
--- combat.c
+++ combat.c
@@ -1,87 +1,85 @@
-#include <stdio.h>
-#include <string.h>
-#include "techniques.h"
-
 #include "code.h"
-#include "code.h"
-
-void phase(Combattant* equipe1,Combattant* equipe2){
-for(int i=0;i<NBPERSO;i++){
-	(equipe1+i)->action+=(equipe1+i)->vitesse;
-	if((equipe1+i)->action>=100){
-		(equipe1+i)->action-=100;
-		tour(equipe1+i,equipe2);
+int cible_valide(Combattant* perso) {
+	if(perso->est_KO==1) {
+		printf("cet ennemi est deja mort \n");
+		return 0;
 	}
-	(equipe2+i)->action+=(equipe2+i)->vitesse;
-	if((equipe2+i)->action>=100){
-		(equipe2+i)->action-=100;
-		tour(equipe2+i,equipe1);
+	else {
+		return 1;
 	}
 }
+
+
+
+
+void phase(Combattant* equipe1,Combattant* equipe2) {
+	for(int i=0; i<NBPERSO; i++) {
+		if((equipe1+i)->est_KO==0) {
+			(equipe1+i)->action+=(equipe1+i)->vitesse;
+			if((equipe1+i)->action>=100) {
+				(equipe1+i)->action-=100;
+				printf("au tour du joueur 1\n");
+				tour(equipe1+i,equipe2);
+			}
+		}
+		else {
+			(equipe1+i)->action=0;
+		}
+		if((equipe2+i)->est_KO==0) {
+			(equipe2+i)->action+=(equipe2+i)->vitesse;
+			if((equipe2+i)->action>=100) {
+				(equipe2+i)->action-=100;
+				printf("au tour du joueur 2\n");
+				tour(equipe2+i,equipe1);
+			}
+		}
+		else {
+			(equipe2+i)->action=0;
+		}
+	}
+	//affichage(equipe1);
+	//affichage(equipe2);
 }
-void tour(Combattant* perso,Combattant* equipe){
+void tour(Combattant* perso,Combattant* equipe) {
 	int choix=-1;
-	while(choix!=1&&choix!=2){
+	while(choix!=1&&choix!=2) {
 		printf("Quelle action voulez vous effectuer:\n 1=attaquer\n 2=capacite speciale\n");
 		scanf("%d",&choix);
-		if(choix!=1&&choix!=2){
+		if(choix!=1&&choix!=2) {
 			printf("mauvais choix");
 		}
 	}
-	if(choix==1){
+	if(choix==1) {
 		attaque(perso,equipe);
 	}
-	else{
+	else {
 		//attaque_speciale(perso,equipe);
 	}
 
 }
-void attaque(Combattant* perso,Combattant* equipe){
-    int choix=-1;
-    while(choix<1||choix>TAILLE_EQUIPE){
-    printf("Entrez la position du perso a attaquer\n");
-    scanf("%d",&choix);
-    }
-    printf("%s attaque %s\n",perso->nom,(equipe+choix-1)->nom);
-    int degats=(perso->attaque)-((equipe+choix-1)->defense);
-    int crit=rand()%100;
-    if(crit<perso->agilite){
-        printf("%s touche un point vital\n",perso->nom);
-        degats=degats*3;// a changer pour equilibrage
-    }// on determine si il y a un coup critique
-    printf("%s recoit %d degats\n",(equipe+choix-1)->nom,degats);
-    (equipe+choix-1)->pv-=degats;
-    if((equipe+choix-1)->pv<=0){
-        printf("%s succombe\n",(equipe+choix-1)->nom);
-        (equipe+choix-1)->est_KO=0;
-    }
-}
+void attaque(Combattant* perso,Combattant* equipe) {
+	int choix=-1;
+	int validation=-1;
+//	affichage(equipe);
+	while(choix<1||choix>TAILLE_EQUIPE||validation!=1) {
+	    
+		printf("Entrez la position du perso a attaquer\n");
+		scanf("%d",&choix);
+		validation=cible_valide(equipe+choix-1);
 
-
-
-
-
-
-
-void appliquer_technique(Combattant* utilisateur, Combattant* cible, TechniqueSpeciale* technique) {
-    if (technique->cooldown_actuel > 0) {
-        printf("La technique %s est en rechargement (%d tours restants).\n",
-               technique->nom, technique->cooldown_actuel);
-        return;
-    }
-
-    printf("%s utilise %s sur %s !\n", utilisateur->nom, technique->nom, cible->nom);
-    printf("Effet : %s\n", technique->description);
-
-    if (strcmp(technique->propriete_affectee, "pv") == 0) {
-        if (strcmp(technique->operation, "-") == 0) {
-            cible->pv -= technique->degat;
-            if (cible->pv < 0) cible->pv = 0;
-        } else if (strcmp(technique->operation, "+") == 0) {
-            cible->pv += technique->degat;
-            if (cible->pv > cible->pv_max) cible->pv = cible->pv_max;
-        }
-    }
-
-    technique->cooldown_actuel = technique->tours_rechargement;
+	}
+	printf("%s attaque %s\n",perso->nom,(equipe+choix-1)->nom);
+	int degats=(perso->attaque)-((equipe+choix-1)->defense);
+	int crit=rand()%100;
+	if(crit<perso->agilite) {
+		printf("%s touche un point vital\n",perso->nom);
+		degats=degats*3;// a changer pour equilibrage
+	}// on determine si il y a un coup critique
+	printf("%s recoit %d degats\n",(equipe+choix-1)->nom,degats);
+	(equipe+choix-1)->pv-=degats;
+	if((equipe+choix-1)->pv<=0) {
+		printf("%s succombe\n",(equipe+choix-1)->nom);
+		(equipe+choix-1)->pv=0;
+		(equipe+choix-1)->est_KO=1;
+	}
 }
