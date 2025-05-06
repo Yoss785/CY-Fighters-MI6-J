14e5b0088132343ede2248e50d1b614dc64701be
Bibi110 <hugo.treny@etu.cyu.fr>
2025-05-06 12:01:41 +0200

    Add files via upload

--- affichage.c
+++ affichage.c
@@ -1,135 +1,128 @@
 #include <stdio.h>
 #include <string.h>
 #include "affichage.h"
+#include "code.h"
 
-// Fonction pour dessiner une barre (PV, vitesse, etc.)
+// Définition des lignes d'affichage
+#define LIGNE_NOM 2
+#define LIGNE_PV 4
+#define LIGNE_BARRE 5
+#define LIGNE_TECH 9
+#define LIGNE_DESC 10
+#define LIGNE_CD 11
+
 void dessiner_barre(int valeur, int max, char symbole) {
     for (int i = 0; i < max; i++) {
-        if (i < valeur) printf("%c", symbole);
-        else printf(" ");
+        putchar((i < valeur) ? symbole : ' ');
     }
 }
 
-// Fonction pour afficher une ligne du plateau
-void afficher_ligne(int ligne, Combattant persos[], int nb_persos) { //ajouter int mode_complet
-    printf("|");
-    int j = 0;
+void afficher_ligne(int ligne, Combattant persos[], int nb_persos, int est_grand_plateau) {
+    char buffer[L + 1];
+    memset(buffer, ' ', L);
+    buffer[L] = '\0';
 
-    while (j < L) {
-        int affiche = 0;
+    putchar('|');
 
-        for (int p = 0; p < nb_persos; p++) {
-            int pos = persos[p].position;
-            if (j == pos) {
-                if (ligne == 2) {
-                    int nb_caracteres = printf("%-20s", persos[p].nom); //%-20s le signe "-" c'est pour aligné à gauche
-                    j += nb_caracteres;
-                    affiche = 1;
-                    break;
-                } 
-                else if (ligne == 4) {
-                    printf("[");
-                    dessiner_barre(persos[p].pv * MAX_BARRE / persos[p].pv_max, MAX_BARRE, '#');
-                    printf("]");
-                    j += MAX_BARRE + 2;
-                    affiche = 1;
-                    break;
-                } 
-                else if (ligne == 5) {
-                    printf("[");
-                    dessiner_barre(persos[p].vitesse * MAX_BARRE / 20, MAX_BARRE, '>');
-                    printf("]");
-                    j += MAX_BARRE + 2;
-                    affiche = 1;
-                    break;
-                } 
-                else if (ligne == 7 /*&& mode_complet*/) {
-                    int nb_caracteres = printf("Attaque normale: faible dégât et aucun temps de rechargement ");
-                    j += nb_caracteres;
-                    affiche = 1;
-                    break;
-                }
-                
-                else if (ligne == 9 /*&& mode_complet*/) {
-                    int nb_caracteres = printf("Technique:: %-15s", persos[p].techniques[0].nom);
-                    j += nb_caracteres;
-                    affiche = 1;
-                    break;
-                }
-                else if (ligne == 10 /*&& mode_complet*/) {
-                    int nb_caracteres = printf("%-40s", persos[p].techniques[0].description);
-                    j += nb_caracteres;
-                    affiche = 1;
-                    break;
-                }
-                else if (ligne == 11 /*&& mode_complet*/) {
-                    int nb_caracteres = printf("CD: %d |Disponible dans %d tour(s)", 
-                        persos[p].techniques[0].tours_rechargement, 
-                        persos[p].techniques[0].cooldown_actuel);
-                    j += nb_caracteres;
-                    affiche = 1;
-                    break;
-                }
+    int espacement = est_grand_plateau ? 25 : 20;
+
+    for (int i = 0; i < nb_persos; i++) {
+        Combattant* perso = &persos[i];
+        if (perso->est_KO) continue;
+
+        int pos_affichage = i * espacement;
+
+        if (pos_affichage < 0 || pos_affichage >= L - 1) continue;
+
+        if (ligne == LIGNE_NOM) {
+            snprintf(buffer + pos_affichage, L - pos_affichage,
+                     perso->est_actif ? "> %-18s <" : "  %-20s", perso->nom);
+        } else if (ligne == LIGNE_PV) {
+            snprintf(buffer + pos_affichage, L - pos_affichage,
+                     "[%3d/%3d]   ", perso->pv, perso->pv_max);
+        } else if (ligne == LIGNE_BARRE) {
+            int nb = (perso->action * MAX_BARRE) / 100;
+            if (nb > MAX_BARRE) nb = MAX_BARRE;
+
+            char barre[MAX_BARRE + 3];
+            barre[0] = '[';
+            for (int j = 0; j < MAX_BARRE; j++)
+                barre[j + 1] = (j < nb) ? '>' : ' ';
+            barre[MAX_BARRE + 1] = ']';
+            barre[MAX_BARRE + 2] = '\0';
+
+            snprintf(buffer + pos_affichage, L - pos_affichage, "%s", barre);
+        }
+
+        if (perso->est_actif && perso->nb_techniques > 0) {
+            if (ligne == LIGNE_TECH) {
+                snprintf(buffer + pos_affichage, L - pos_affichage, "Technique: %-20s", perso->techniques[0].nom);
+            } else if (ligne == LIGNE_DESC) {
+                snprintf(buffer + pos_affichage, L - pos_affichage, "%.*s",
+                         (L - pos_affichage > 70) ? 70 : (L - pos_affichage - 1),
+                         perso->techniques[0].description);
+            } else if (ligne == LIGNE_CD) {
+                snprintf(buffer + pos_affichage, L - pos_affichage,
+                         "CD: %2d | Dispo dans %2d tour(s)",
+                         perso->techniques[0].tours_rechargement,
+                         perso->techniques[0].cooldown_actuel);
             }
         }
+    }
 
-        if (!affiche) {
-            printf(" ");
-            j++;
+    printf("%s|\n", buffer);
+}
+
+void afficher_plateau_petit(Combattant persos[], int nb_persos) {
+    putchar(' ');
+    for (int i = 0; i < L; i++) putchar('_');
+    putchar('\n');
+
+    for (int l = 1; l <= h; l++)
+        afficher_ligne(l, persos, nb_persos, 0);
+
+    putchar(' ');
+    for (int i = 0; i < L; i++) putchar('_');
+    putchar('\n');
+}
+
+void afficher_plateau_grand(Combattant persos[], int nb_persos) {
+    putchar(' ');
+    for (int i = 0; i < L; i++) putchar('_');
+    putchar('\n');
+
+    for (int l = 1; l <= H; l++)
+        afficher_ligne(l, persos, nb_persos, 1);
+
+    putchar(' ');
+    for (int i = 0; i < L; i++) putchar('_');
+    putchar('\n');
+}
+
+void afficher_plateau(Combattant equipe1[], Combattant equipe2[]) {
+    Combattant* actif = NULL;
+    int equipe1_active = 0;
+
+    for (int i = 0; i < TAILLE_EQUIPE; i++) {
+        if (equipe1[i].est_actif) {
+            actif = &equipe1[i];
+            equipe1_active = 1;
+            break;
+        } else if (equipe2[i].est_actif) {
+            actif = &equipe2[i];
+            break;
         }
     }
-    printf("|\n");
-}
 
-// Fonction pour afficher le plateau en petit 
-void afficher_plateau_petit(Combattant persos[], int nb_persos) {
-    printf(" ");
-    for (int i = 0; i < L; i++) printf("_");
-    printf("\n");
-
-    for (int i = 1; i <= h; i++) {
-        afficher_ligne(i, persos, nb_persos); //ajouter "0" pour que mode_complet=0
-    }
-
-    printf(" ");
-    for (int i = 0; i < L; i++) printf("_");
-    printf("\n");
-}
-
-// Fonction pour afficher le plateau en grand
-void afficher_plateau_grand(Combattant persos[], int nb_persos) {
-    printf(" ");
-    for (int i = 0; i < L; i++) printf("_");
-    printf("\n");
-
-    for (int i = 1; i <= H; i++) {
-        afficher_ligne(i, persos, nb_persos); //ajouter"1" pour que mode_complet=1
-    }
-
-    printf(" ");
-    for (int i = 0; i < L; i++) printf("_");
-    printf("\n");
-}
-
- //Fonction pour afficher les deux plateaux (le plateau de l'équipe 2 s'affiche pas car trop d'informations affichées)
-void afficher_plateau(Combattant equipe1[], Combattant equipe2[]) {
-    printf("\n=== Plateau Équipe 1 ===\n");
-    afficher_plateau_grand(equipe1, TAILLE_EQUIPE);
-
-    printf("\n=== Plateau Équipe 2 ===\n");
-    afficher_plateau_petit(equipe2, TAILLE_EQUIPE);
-}
-/* void afficher_plateaux_selon_tour(Combattant equipe1[], Combattant equipe2[], int tour_equipe) {
-    if (tour_equipe == 1) {
-        printf("\n=== Tour de l'Équipe 1 ===\n");
+    if (equipe1_active) {
+        printf("=== ÉQUIPE 1 (actif) ===\n");
         afficher_plateau_grand(equipe1, TAILLE_EQUIPE);
+        printf("=== ÉQUIPE 2 ===\n");
         afficher_plateau_petit(equipe2, TAILLE_EQUIPE);
-    } else if (tour_equipe == 2) {
-        printf("\n=== Tour de l'Équipe 2 ===\n");
-        afficher_plateau_grand(equipe2, TAILLE_EQUIPE);
-        afficher_plateau_petit(equipe1, TAILLE_EQUIPE);
     } else {
-        printf("Erreur : numéro d'équipe invalide (tour_equipe = %d)\n", tour_equipe);
+        printf("=== ÉQUIPE 2 (actif) ===\n");
+        afficher_plateau_grand(equipe2, TAILLE_EQUIPE);
+        printf("=== ÉQUIPE 1 ===\n");
+        afficher_plateau_petit(equipe1, TAILLE_EQUIPE);
     }
 }
-*/
--- affichage.h
+++ affichage.h
@@ -4,7 +4,7 @@
 #include "code.h"
 
 void dessiner_barre(int valeur, int max, char symbole);
-void afficher_ligne(int ligne, Combattant persos[], int nb_persos);//ajouter int mode_complet
+void afficher_ligne(int ligne, Combattant persos[], int nb_persos,int est_grand_plateau);
 void afficher_plateau_petit(Combattant persos[], int nb_persos);
 void afficher_plateau_grand(Combattant persos[], int nb_persos);
 void afficher_plateau(Combattant equipe1[], Combattant equipe2[]);
