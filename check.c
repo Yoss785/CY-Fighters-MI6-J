b52f998ebfe202bf09bdc3754f14777797e94cb4
Grandjean Yanis <yanisgrandjean2005@gmail.com>
2025-05-08 15:34:32 +0200

    Update check.c

--- check.c
+++ check.c
@@ -1,21 +1,30 @@
-#include "code.h"
-int equipe_vivante(Combattant* equipe){
-    int morts=0;
-    for(int i=0;i<TAILLE_EQUIPE;i++){
-        if((equipe+i)->est_KO==1){
-            morts+=1;
-        }
-    }
-    if(morts==TAILLE_EQUIPE){
-        return 0;
-    }
-    else{
-        return 1;
-    }
+#include <stdio.h>
+#include <stdlib.h>
+#include "check.h"
+
+void flush(void) {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
 }
-void flush(){
-    char lettre='a';
-    while(lettre=!'\n'){
-        scanf("%c",&lettre);
+
+int getInt(int min, int max) {
+    int valeur;
+    char buffer[100];
+
+    while (1) {
+        if (fgets(buffer, sizeof(buffer), stdin)) {
+            if (sscanf(buffer, "%d", &valeur) == 1) {
+                if (valeur >= min && valeur <= max) {
+                    return valeur;
+                } else {
+                    printf("Erreur : valeur hors des limites.\n");
+                }
+            } else {
+                printf("Erreur : entrée invalide, veuillez entrer un entier.\n");
+            }
+        } else {
+            printf("Erreur de lecture. Réessayez.\n");
+            clearerr(stdin);
+        }
     }
 }
