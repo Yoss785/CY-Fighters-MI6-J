74ae126b71453872d4faa4a77cdaeb368248e521
Bibi110 <hugo.treny@etu.cyu.fr>
2025-05-09 09:13:03 +0200

    Update techniques.h

--- techniques.h
+++ techniques.h
@@ -15,4 +15,8 @@ void blaster_meteor(Combattant* utilisateur, Combattant* cible);
 void mur_de_glace(Combattant* utilisateur, Combattant* cible);
 void final_flash(Combattant* utilisateur, Combattant* cible);
 void kamehameha(Combattant* utilisateur, Combattant* cible);
+void crazy_diamond(Combattant* utilisateur, Combattant* cible);
+void appliquer_technique_allie(Combattant* utilisateur,Combattant* cible,Combattant* technique);
+void illumination_divine(Combattant* utilisateur, Combattant* cible);
+void senzu(Combattant* utilisateur, Combattant* cible);
 #endif
