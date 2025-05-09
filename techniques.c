a0aa49dbf0909a5594186ba698b1e953a3949b93
Bibi110 <hugo.treny@etu.cyu.fr>
2025-05-09 09:10:13 +0200

    Add files via upload

--- techniques.c
+++ techniques.c
@@ -1,26 +1,92 @@
-#include <stdio.h>
-#include <string.h>
 #include "techniques.h"
+#include "code.h"
+#include "combat.h"
+
+
+void attaque_speciale(Combattant* utilisateur, Combattant* equipe_adverse) {
+	TechniqueSpeciale* tech = &utilisateur->techniques[0];
+
+	if (tech->cooldown_actuel > 0) {
+		printf("La technique %s est encore en rechargement (%d tours restants).\n",
+		       tech->nom, tech->cooldown_actuel);
+		return;
+	}
+
+	printf("Choisissez un ennemi pour utiliser la technique spC)ciale %s :\n", tech->nom);
+	for (int i = 0; i < TAILLE_EQUIPE; i++) {
+		if (!equipe_adverse[i].est_KO) {
+			printf("%d. %s (PV: %d)\n", i + 1, equipe_adverse[i].nom, equipe_adverse[i].pv);
+		}
+	}
+
+	int choix = -1, validation = 0;
+	while (choix < 1 || choix > TAILLE_EQUIPE || validation != 1) {
+		scanf("%d", &choix);
+		validation = cible_valide(&equipe_adverse[choix - 1]);
+	}
+
+	appliquer_technique(utilisateur, &equipe_adverse[choix - 1], tech);
+}
+
+
 
 void appliquer_technique(Combattant* utilisateur, Combattant* cible, TechniqueSpeciale* technique) {
-    if (technique->cooldown_actuel > 0) {
-        printf("La technique %s est en rechargement (%d tours restants).\n",
-               technique->nom, technique->cooldown_actuel);
-        return;
-    }
 
-    printf("%s utilise %s sur %s !\n", utilisateur->nom, technique->nom, cible->nom);
-    printf("Effet : %s\n", technique->description);
+	if(strcmp(technique->nom,"Mur de glace")==0) {
+		mur_de_glace(utilisateur,cible);
+	}
+	else if(strcmp(technique->nom,"Blaster meteor")==0) {
+		blaster_meteor(utilisateur,cible);
+	}
+	else if(strcmp(technique->nom,"Kamehameha")==0) {
+		kamehameha(utilisateur,cible);
+	}
+	else if(strcmp(technique->nom,"Final flash")==0) {
+		final_flash(utilisateur,cible);
+	}
 
-    if (strcmp(technique->propriete_affectee, "pv") == 0) {
-        if (strcmp(technique->operation, "-") == 0) {
-            cible->pv -= technique->degat;
-            if (cible->pv < 0) cible->pv = 0;
-        } else if (strcmp(technique->operation, "+") == 0) {
-            cible->pv += technique->degat;
-            if (cible->pv > cible->pv_max) cible->pv = cible->pv_max;
-        }
-    }
 
-    technique->cooldown_actuel = technique->tours_rechargement;
+
+	printf("%s utilise %s sur %s !\n", utilisateur->nom, technique->nom, cible->nom);
+	printf("Effet : %s\n", technique->description);
+
+	technique->cooldown_actuel = technique->tours_rechargement;
+}
+
+void mur_de_glace(Combattant* utilisateur, Combattant* cible) {
+	printf("ICE AGE\n");
+	cible->pv-=utilisateur->attaque;
+	if(cible->pv<=0) {
+		printf("%s transforme %s en statue\n",utilisateur->nom,cible->nom);
+	}
+	cible->action=0;
+}
+
+void blaster_meteor(Combattant* utilisateur, Combattant* cible) {
+	printf("Chargement du KI\n");
+	if(cible->position==1) {
+		attaque(utilisateur,cible);
+		attaque(utilisateur,cible+1);
+	}
+	if(cible->position==2) {
+		attaque(utilisateur,cible-1);
+		attaque(utilisateur,cible);
+		attaque(utilisateur,cible+1);
+	}
+	if(cible->position==3) {
+		attaque(utilisateur,cible-1);
+		attaque(utilisateur,cible);
+	}
+}
+void kamehameha(Combattant* utilisateur, Combattant* cible) {
+	printf("Chargement du KI\n");
+	printf("KAMEHAME..........HAAAAAAAAAAA\n");
+	attaque(utilisateur,cible);
+	utilisateur->attaque*=1.2;
+}
+void final_flash(Combattant* utilisateur, Combattant* cible) {
+	printf("Chargement du KI\n");
+	printf("FINAL FLASH\n");
+	attaque(utilisateur,cible);
+	utilisateur->defense*=1.2;
 }
--- techniques.h
+++ techniques.h
@@ -1,9 +1,18 @@
-// techniques.h
 #ifndef TECHNIQUES_H
 #define TECHNIQUES_H
 
 #include "code.h"
 
-void appliquer_technique(Combattant* utilisateur, Combattant* cible, TechniqueSpeciale* technique);
 
+
+
+
+
+
+
+void appliquer_technique(Combattant* utilisateur, Combattant* cible, TechniqueSpeciale* technique);
+void blaster_meteor(Combattant* utilisateur, Combattant* cible);
+void mur_de_glace(Combattant* utilisateur, Combattant* cible);
+void final_flash(Combattant* utilisateur, Combattant* cible);
+void kamehameha(Combattant* utilisateur, Combattant* cible);
 #endif
