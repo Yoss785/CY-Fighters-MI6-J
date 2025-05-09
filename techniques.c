6838b9226da4e796596fa377f8c02d7e8e126ec5
Bibi110 <hugo.treny@etu.cyu.fr>
2025-05-09 09:11:51 +0200

    Update techniques.c

--- techniques.c
+++ techniques.c
@@ -5,14 +5,17 @@
 
 void attaque_speciale(Combattant* utilisateur, Combattant* equipe_adverse) {
 	TechniqueSpeciale* tech = &utilisateur->techniques[0];
-
+    int cible;
+    printf("voulez vous utiliser la technique sur un ennemi(tapez 1)ou un allie(tapez 2)\n");
+    scanf("%d",&cible);
 	if (tech->cooldown_actuel > 0) {
 		printf("La technique %s est encore en rechargement (%d tours restants).\n",
 		       tech->nom, tech->cooldown_actuel);
 		return;
 	}
-
-	printf("Choisissez un ennemi pour utiliser la technique spC)ciale %s :\n", tech->nom);
+	
+    if(cible==1){
+	printf("Choisissez un ennemi pour utiliser la technique speciale %s :\n", tech->nom);
 	for (int i = 0; i < TAILLE_EQUIPE; i++) {
 		if (!equipe_adverse[i].est_KO) {
 			printf("%d. %s (PV: %d)\n", i + 1, equipe_adverse[i].nom, equipe_adverse[i].pv);
@@ -26,6 +29,26 @@ void attaque_speciale(Combattant* utilisateur, Combattant* equipe_adverse) {
 	}
 
 	appliquer_technique(utilisateur, &equipe_adverse[choix - 1], tech);
+    }
+    
+    if(cible==2){
+        Combattant* equipe_allie=utilisateur->equipe;
+	printf("Choisissez un allie pour utiliser la technique speciale %s :\n", tech->nom);
+	for (int i = 0; i < TAILLE_EQUIPE; i++) {
+		if (!equipe_allie[i].est_KO) {
+			printf("%d. %s (PV: %d)\n", i + 1, equipe_allie[i].nom, equipe_allie[i].pv);
+		}
+	}
+
+	int choix = -1, validation = 0;
+	while (choix < 1 || choix > TAILLE_EQUIPE || validation != 1) {
+		scanf("%d", &choix);
+		validation = cible_valide(&equipe_allie[choix - 1]);
+	}
+
+	appliquer_technique(utilisateur, &equipe_allie[choix - 1], tech);
+    }
+    
 }
 
 
@@ -44,6 +67,9 @@ void appliquer_technique(Combattant* utilisateur, Combattant* cible, TechniqueSp
 	else if(strcmp(technique->nom,"Final flash")==0) {
 		final_flash(utilisateur,cible);
 	}
+	else if(strcmp(technique->nom,"Illumination divine")==0) {
+		illumination_divine(utilisateur,cible);
+	}
 
 
 
@@ -51,11 +77,22 @@ void appliquer_technique(Combattant* utilisateur, Combattant* cible, TechniqueSp
 	printf("Effet : %s\n", technique->description);
 
 	technique->cooldown_actuel = technique->tours_rechargement;
+
+}
+
+void appliquer_technique_allie(Combattant* utilisateur,Combattant* cible,Combattant* technique){
+    if(strcmp(technique->nom,"Crazy Diamond")==0) {
+		crazy_diamond(utilisateur,cible);
+    }
+    else if(strcmp(technique->nom,"Senzu")==0) {
+		senzu(utilisateur,cible);
+    }
 }
 
 void mur_de_glace(Combattant* utilisateur, Combattant* cible) {
 	printf("ICE AGE\n");
 	cible->pv-=utilisateur->attaque;
+	printf("Aokiji inflige %d degats a %s\n",utilisateur->attaque,cible->nom);
 	if(cible->pv<=0) {
 		printf("%s transforme %s en statue\n",utilisateur->nom,cible->nom);
 	}
@@ -82,11 +119,46 @@ void kamehameha(Combattant* utilisateur, Combattant* cible) {
 	printf("Chargement du KI\n");
 	printf("KAMEHAME..........HAAAAAAAAAAA\n");
 	attaque(utilisateur,cible);
-	utilisateur->attaque*=1.2;
+	utilisateur->buff_attaque+=2;
 }
 void final_flash(Combattant* utilisateur, Combattant* cible) {
 	printf("Chargement du KI\n");
 	printf("FINAL FLASH\n");
 	attaque(utilisateur,cible);
-	utilisateur->defense*=1.2;
+	utilisateur->buff_defense+=2;
+}
+void crazy_diamond(Combattant* utilisateur, Combattant* cible){
+    printf("Josuke pose sa main sur %s\n",cible->nom);
+    cible->pv=cible->pv_max;
+    printf("%s se sent de nouveau en pleine forme\n",cible->nom);
+}
+void grande_eruption(Combattant* utilisateur, Combattant* cible){
+    printf("Akainu charge son magma\n");
+    printf("GRANDE ERRUPTION\n");
+    int degats=utilisateur->attaque-cible->defense;
+    degats*=2;
+    printf("Akainu inflige %d degats a %s\n",degats,cible->nom);
+    if(cible->pv<=0) {
+		printf("%s transforme %s en tas de cendre\n",utilisateur->nom,cible->nom);
+	}
+	else{
+	    cible->brulure+=3;
+	}
+}
+void illumination_divine(Combattant* utilisateur, Combattant* cible){
+    printf("Kizaru place son index et son majeur devant les yeux de %s \n",cible->nom);
+    printf("AMATERASU: ILLUMINATION DIVINE\n");
+    int degats=utilisateur->attaque-cible->defense;
+    degats*=1.5;
+    printf("Kizaru inflige %d degats a %s\n",degats,cible->nom);
+    if(cible->pv<=0) {
+		printf("%s fait exploser %s \n",utilisateur->nom,cible->nom);
+	}
+}
+void senzu(Combattant* utilisateur, Combattant* cible){
+    printf("Krillin lance un haricot a %s",cible->nom);
+    cible->pv+=40;
+    cible->brulure=0;
+    printf("%s se sent de nouveau en pleine forme\n",cible->nom);
+}
 }
