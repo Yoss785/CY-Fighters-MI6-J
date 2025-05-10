b6d53beb8ad9552cba5218b547c28ff8099a786d
Grandjean Yanis <yanisgrandjean2005@gmail.com>
2025-05-10 23:52:17 +0200

    Update selection.h

--- selection.h
+++ selection.h
@@ -5,7 +5,7 @@
 #include "code.h" 
 
 void afficher_stats(Combattant perso);
-void afficher_equipe(Combattant* equipe);
+void afficher_equipe_selection(Combattant* equipe);
 void apercu(FILE* fichier, int choix);
 void construction_perso(Combattant* perso, FILE* fichier);
 void selection(Combattant* equipe1, Combattant* equipe2, FILE* fichier);
