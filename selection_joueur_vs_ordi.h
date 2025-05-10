1e4cc70e1f66de2738911bf5b5ca7641dcb70222
nyxro09 <169052725+nyxro09@users.noreply.github.com>
2025-05-10 11:52:45 +0200

    Create selection_joueur_vs_ordi.h

---
+++ selection_joueur_vs_ordi.h
@@ -0,0 +1,12 @@
+#ifndef SELECTION_JVO_H
+#define SELECTION_JVO_H
+
+#include <stdio.h>
+#include "code.h" 
+#include "selection.h"
+
+void selection_joueur_vs_ordi(Combattant* joueur, Combattant* ordinateur, FILE* fichier);
+void construction_perso_auto(Combattant* perso, FILE* fichier);
+
+
+#endif
