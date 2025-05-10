8d56afc1cd194dfe5412355a9f775a5a439cdcbe
Grandjean Yanis <yanisgrandjean2005@gmail.com>
2025-05-10 23:46:56 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -3,9 +3,10 @@
 
 #include "code.h"
 
-void phase(Combattant* equipe1,Combattant* equipe2);
-void tour(Combattant* perso,Combattant* equipe);
-void attaque(Combattant* perso,Combattant* equipe);
-void appliquer_technique(Combattant* utilisateur, Combattant* cible, TechniqueSpeciale* technique);
+void phase(Combattant* equipe1, Combattant* equipe2);
+void tour(Combattant* perso, Combattant* equipe);
+void attaque(Combattant* perso, Combattant* equipe);
+int cible_valide(Combattant* perso);
+int equipe_vivante(Combattant* equipe);
 
 #endif
